226d9d619db03fc53874ed1881ffeb4324ed1a08
muckity <andrewgoldstein@hotmail.com>
2022-04-21 05:37:46 -0500

    fixed layout width

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,9 @@ import React from "react"
 export default function Header() {
     return (
         <header className="header">
-            <img 
-                src="./images/troll-face.png" 
-                className="header--image"
-            />
+
             <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
+            <h4 className="header--project">Muckitymuck</h4>
         </header>
     )
 }
